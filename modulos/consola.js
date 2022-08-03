e1a7507289f90fc084e04e50bda140bf8dd9439b
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-08-03 11:21:51 -0600

    consola

---
+++ modulos/consola.js
@@ -0,0 +1,25 @@
+console.log('algo');
+console.info('algo');
+console.error('algo');
+console.warn('algo');
+let tabla = [
+	{
+		a: 1,
+		b: 'Z'
+	},
+	{
+		a: 2,
+		b: 'X'
+	}
+]
+console.log(tabla)
+console.table(tabla)
+console.group('conversacion')
+console.log('hola');
+console.log('dummy text');
+console.log('adios');
+console.groupEnd('conversacion');
+console.count('veces')
+console.count('veces')
+console.count('veces')
+console.countReset('veces')
