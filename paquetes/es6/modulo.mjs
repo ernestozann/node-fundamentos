dac2a052824d04f7420bf0aecd53cc9f95c4b70f
Ernesto Martinez '3 <ernestomm99@hotmail.com>
2022-08-08 11:13:10 -0600

    construyendo modulos: require e import

---
+++ paquetes/es6/modulo.mjs
@@ -0,0 +1,8 @@
+function saludar() {
+	console.log('hello world');
+}
+
+export default {
+	saludar,
+	prop1: "era un modulo experimental"
+}
---
+++ paquetes/modulo.js
@@ -0,0 +1,8 @@
+function saludar() {
+	console.log('hello world');
+}
+
+module.exports = {
+	saludar,
+	prop1: 'Hola que tal'
+}
